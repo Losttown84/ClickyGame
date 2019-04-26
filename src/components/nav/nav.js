8b2a8a8359dbde3bf736cd84a8a96be74ffee73c
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-04-25 22:32:24 -0500

    Nav made with score prop and then exported

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,2 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "./nav.css";
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <ul>
+          <li className="itemLeft">Check Your Memory</li>
+          <li className="itemCenter"></li>
+          <li className="itemRight">Score: {this.props.score}</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
