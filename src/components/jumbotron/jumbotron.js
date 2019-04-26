a2d9f76a856351a7dad8ec48685f3fa7e9549c22
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-04-25 21:45:15 -0500

    jumbotron passed through with a title and description

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -1,2 +1,11 @@
 import React from "react";
 import "./jumbotron.css";
+
+const Jumbotron = () => (
+  <header className = "header">
+    <h1>Metsy Clicky Game</h1>
+    <h2>Click a pic to get a point, without clicking the same pic!</h2>
+  </header>
+);
+
+export default Jumbotron;
