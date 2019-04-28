16230fb489d1557d8fb06115ec5dbc7c68345876
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-04-28 11:02:00 -0500

    redoing appjs file with picture clicking and importing components

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Nav from ".components/nav";
+import Jumbotron from ".components/Jumbotron";
+import Playercard from ".components/Playercard";
+import footer from ".components/footer";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    player,
+    playerClicked: [],
+    score: 0
+  };
+
+  imageClick = event => {
+    const currentPlayer = event.target.alt;
+    const clickedPlayer = this.state.playerClicked.indexOf(currentPlayer) > -1;
+
+    if(clickedPlayer) {
+      this.setState({
+        player: this.state.player.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        playerClicked: [],
+        score: 0
+      });
+      alert("You chose that picture already, so you've lost. Try again?");
+    } else {
+      this.setState(
+        {
+          player: this.state.player.sort(function(a, b) {
+            return 0.5 - Math.random();
+          })
+        }
+      )
+    }
+  }
+}
